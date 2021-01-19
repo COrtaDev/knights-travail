494151066330debf671d43de1bd744f5e89a6cd1
Conrad <conradorta@gmail.com>
2021-01-19 13:29:26 -0500

    finished class, trying to get all tests to pass.

--- tree.py
+++ tree.py
@@ -30,8 +30,16 @@ class Node:
     def parent(self, node):
         if self._parent == node:
             return
-
         if self._parent != None:
             self._parent.remove_child(self)
         if self._parent != None:
             self._parent.add_child(self)
+
+    def depth_search(self, value):
+        if self.value == value:
+            return self
+        for i in range(len(self.children)):
+            child = self.children[i]
+            result = child.depth_search(value)
+            if result != None:
+                return result
